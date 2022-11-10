e1c1ecbadd5da001a947264db369a92369893acf
Mateus Jesus <mateusjdev@gmail.com>
2022-11-10 13:27:41 -0300

    feat: completeOrder e sectionTitle do formulário

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { DefaultLayout } from "./layouts/DefaultLayout";
+import { CompleteOrderPage } from './pages/CompleteOrder';
 import { HomePage } from './pages/Home';
 
 export function Router() {
@@ -7,6 +8,8 @@ export function Router() {
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/completeOrder" element={<CompleteOrderPage />} />
+        {/* <Route path="/orderConfirmed" element={<OrderConfirmedPage />} /> */}
       </Route>
     </Routes>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,9 +17,11 @@ export function Header() {
             <MapPin size={20} weight="fill" />
             Porto Alegre, RS
           </HeaderButton>
-          <HeaderButton variant="yellow">
-            <ShoppingCart size={20} weight="fill" />
-          </HeaderButton>
+          <NavLink to="/completeOrder">
+            <HeaderButton variant="yellow">
+              <ShoppingCart size={20} weight="fill" />
+            </HeaderButton>
+          </NavLink>
         </HeaderButtonsContainer>
       </div>
     </HeaderContainer>
